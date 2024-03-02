a6505054d7d577ac0daf6debd66ef28eef5d5b20
SiakLangAtoy <159037114+SiakLangAtoy@users.noreply.github.com>
2024-03-02 22:31:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wirwo_20/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.wirwo_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    LineChart mpLineChart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mpLineChart=(LineChart) findViewById(R.id.line_chart);
+        LineDataSet lineDataSet1 = new LineDataSet(dataValues1(), "Data Set 1");
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(lineDataSet1);
+
+        LineData data = new LineData(dataSets);
+        mpLineChart.setData(data);
+        mpLineChart.invalidate();
+    }
+private ArrayList<Entry> dataValues1()
+{
+        ArrayList<Entry> dataVals = new ArrayList<Entry>();
+        dataVals.add(new Entry(1,27));
+        dataVals.add(new Entry(2,24));
+        dataVals.add(new Entry(3,2));
+        dataVals.add(new Entry(4,0));
+        dataVals.add(new Entry(5,20));
+
+        return dataVals;
+
+
+}
+}
